9f5778199933da394720565ff245c69eafde87f3
armansk15 <no-reply>
2022-05-08 13:52:52 +0300

    Add and store message

--- script.js
+++ script.js
@@ -3,7 +3,31 @@ console.log(generateRandomNumber);
 // Gets # from 0
 
 const collectiveWisdom = {
-    signInfo: ['sun', 'moon', 'star', 'comet'];
-    fortuneOutput: ['awful luck', 'bad luck', 'you\'re fine', 'good luck', 'great luck'];
-    advice: ['be more positive', 'change your mindset', 'listen to more trap', 'care less'];
+    signInfo: ['sun', 'moon', 'star', 'comet'],
+    fortuneOutput: ['awful luck', 'bad luck', 'you\'re fine', 'good luck', 'great luck'],
+    advice: ['be more positive', 'change your mindset', 'listen to more trap', 'care less']
+};
+
+// Store the 'wisdom' in an array
+let personalWisdom = []
+
+// Store message components and create the message
+for(let smh in collectiveWisdom) {
+    let thisIndex = generateRandomNumber(collectiveWisdom[smh].length)
+
+    switch (smh) {
+        case 'signInfo':
+            personalWisdom.push(`Your sign is: ${collectiveWisdom[smh][thisIndex]}`)
+            break
+        case 'fortuneOutput':
+            personalWisdom.push(`Your output is: ${collectiveWisdom[smh][thisIndex]}`)
+            break
+        case 'advice':
+            personalWisdom.push(`You should: ${collectiveWisdom[smh][thisIndex]}`)
+            break
+        default:
+            personalWisdom.push('Not enough info!')
+    }
 }
+
+
