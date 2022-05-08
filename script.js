37a70cce7db14f10b304a12284862ae164437be9
armansk15 <no-reply>
2022-05-08 13:35:06 +0300

    Add message components

---
+++ script.js
@@ -0,0 +1,9 @@
+const generateRandomNumber = num => Math.floor(Math.random() * num);
+console.log(generateRandomNumber);
+// Gets # from 0
+
+const collectiveWisdom = {
+    signInfo: ['sun', 'moon', 'star', 'comet'];
+    fortuneOutput: ['awful luck', 'bad luck', 'you\'re fine', 'good luck', 'great luck'];
+    advice: ['be more positive', 'change your mindset', 'listen to more trap', 'care less'];
+}
